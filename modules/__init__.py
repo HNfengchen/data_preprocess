35d19894d822e04fec8bb5145fec79d41ab88c13
lihao <lihao@zonechangestech.com>
2026-04-16 10:00:27 +0800

    feat: update modules __init__ with imports

---
+++ modules/__init__.py
@@ -0,0 +1,5 @@
+from .deduplication import DeduplicationModule
+from .deblur import DeblurModule
+from .anomaly_detection import AnomalyDetectionModule
+
+__all__ = ["DeduplicationModule", "DeblurModule", "AnomalyDetectionModule"]
